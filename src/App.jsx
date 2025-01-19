1997f2afb715944c24f108504ebfd47057025430
Vivek Kamble <mrvivekkamble8@gmail.com>
2025-01-19 13:36:56 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    
+       
+      <p className="read-the-docs">
+        Under Development
+      </p>
+    </>
+  )
+}
+
+export default App
